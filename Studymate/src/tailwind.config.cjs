71ad9c124cb688203363535cdb033da3e6238659
Mahesh <maheshmetre2005@gmail.com>
2025-10-14 16:10:11 +0530

    updated

--- Studymate/src/tailwind.config.cjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./index.html', './src/**/*.{js,jsx}'], // scans all React files
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
