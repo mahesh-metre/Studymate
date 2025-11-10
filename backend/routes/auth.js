1d20e5b201d3d89cbe4ccd54e7c5e2623b7f8aeb
Dhathri44 <dhathrimundlapudi44@gmail.com>
2025-11-11 00:09:47 +0530

    change

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,7 @@
 import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import pool from "../db.js";
+import { pool } from "../server.js";
 import dotenv from "dotenv";
 import nodemailer from "nodemailer";
 
