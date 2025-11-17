de83f594b358a6ed5baa066a0d260882f5da68e7
Dhathri <dhathrimundlapudi44@gmail.com>
2025-11-17 11:22:06 +0530

    Add html2canvas and GIF.js imports to python.jsx

--- src/frontend/python.jsx
+++ src/frontend/python.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import html2canvas from "html2canvas";
+import GIF from "gif.js.optimized";
 
 
 // --- CSS (GlobalStyles Component) ---
